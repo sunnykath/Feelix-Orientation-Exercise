2d7474571955d4d0324b058febe828fef37d2f1b
Suyash Kathuria <suyash.kathuria@myob.com>
2023-07-04 20:23:41 +1000

    Add router, homepage and clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@myob/myob-widgets";
 import InvoiceDetailView from "./InvoiceDetail/InvoiceDetailView";
-import InvoicesListView from './InvoiceList/InvoicesListView';
+import InvoicesListView from "./InvoiceList/InvoicesListView";
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <ThemeProvider theme="classic">
-        {/* <InvoicesListView/> */}
-        <InvoiceDetailView />
-      </ThemeProvider>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ThemeProvider theme="classic">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/list" element={<InvoicesListView />}/>
+            <Route path="/detail" element={<InvoiceDetailView />}/>
+          </Routes>
+        </ThemeProvider>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,68 @@
+import {
+  Box,
+  ButtonLink,
+  Card,
+  ExpandIcon,
+  Heading,
+  ListViewIcon,
+  OpenExternalLinkIcon,
+  PageHead,
+  TextLink,
+} from "@myob/myob-widgets";
+import { Navigate, useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigateTo = useNavigate();
+
+  const callToAction = [
+    <ButtonLink icon={<ListViewIcon />} onClick={() => navigateTo("/list")}>
+      List View
+    </ButtonLink>,
+    <ButtonLink icon={<ExpandIcon />} onClick={() => navigateTo("/detail")}>
+      Detail View
+    </ButtonLink>,
+  ];
+
+  const CardHeader = () => (
+    <PageHead title="Suyash's Orientation Exercise">
+      Find detailed feedback in
+      <TextLink
+        target="self"
+        href="https://myobconfluence.atlassian.net/wiki/spaces/DesignSystem/pages/9156463985/Suyash+Kahthuria"
+        icon={<OpenExternalLinkIcon />}
+      >
+        Confluence
+      </TextLink>
+    </PageHead>
+  );
+
+  const CardBody = () => (
+    <Box>
+      <Box display="flex" flexDirection="column">
+        <Heading>Pages</Heading>
+        {callToAction}
+      </Box>
+      <Box as="p" style={{marginTop:"2rem"}}>
+        <Heading>Main Pointers</Heading>
+        <ul>
+          <li>Box Props</li>
+          <li>Resize Observer</li>
+          <li>The term Fluid</li>
+          <li>Don’t need the div wrappers in some components! </li>
+          <li>Straightforward/Simple - ToGo Example</li>
+          <li>Table Item</li>
+          <li>Table - Child implementation [more options] </li>
+        </ul>
+      </Box>
+    </Box>
+  );
+
+  return (
+    <div style={{ background: "#ebeef1", padding: "2.4rem" }}>
+      <Card
+        header={<Card.Header child={<CardHeader />} />}
+        body={<Card.Body child={<CardBody />} />}
+      />
+    </div>
+  );
+}
